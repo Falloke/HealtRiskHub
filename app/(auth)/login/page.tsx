6cf16eb7e86295a5548203164be6e9b6b665eb03
tontanagon <tontanagon555@gmail.com>
2025-08-23 21:08:16 +0700

    create input login form

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -11,9 +11,9 @@ export default function LoginPage() {
   setUser({ name: "Pimonpan Doungtip", email: "pimonpandt@gmail.com" });
 
   const [formData, setFormData] = useState({
-    email:"",
-    password:""
-  })
+    email: "",
+    password: "",
+  });
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-pink-100">
@@ -26,8 +26,26 @@ export default function LoginPage() {
             เข้าสู่ระบบ
           </h2>
           <form className="space-y-6">
-            <Input placeholder="Email" type="email" />
-            <Input placeholder="Password" type="password" value={formData.email} onChange={(e) => setFormData({...formData, email:e.currentTarget.value})}/>
+            {/* <Input placeholder="Email" type="email" />
+            <Input placeholder="Password" type="password" value={formData.email} onChange={(e) => setFormData({...formData, email:e.currentTarget.value})}/> */}
+
+            <input
+              className="border-input focus-visible:ring-primary-pink flex h-10 w-full rounded-md border bg-white px-3 py-2 text-sm ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-400 focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50"
+              type="email"
+              value={formData.email}
+              onChange={(e) =>
+                setFormData({ ...formData, email: e.currentTarget.value })
+              }
+            />
+            <input
+              className="border-input focus-visible:ring-primary-pink flex h-10 w-full rounded-md border bg-white px-3 py-2 text-sm ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-400 focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50"
+              type="password"
+              value={formData.email}
+              onChange={(e) =>
+                setFormData({ ...formData, password: e.currentTarget.value })
+              }
+            />
+
             <div className="text-center">
               <Button className="bg-primary-pink w-full text-white hover:bg-pink-500">
                 สมัครสมาชิก
