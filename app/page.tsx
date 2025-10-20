7038be1c1564bc8a54464bcf2f6871be32031ffa
Falloke <tossapontansupan9@gmail.com>
2025-10-21 00:12:41 +0700

    edit: polish components & fix imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
 import { Icons } from "@/app/icons";
 import { iconPaths } from "@/app/icons";
-import DashBordHeader from "./features/main/dashBoardPage/component/DashBoardHeader";
-import HomePageRender from "@/app/(main)/home/page";
+//import DashBordHeader from "./features/main/dashBoardPage/component/DashBoardHeader";
+import HomePageRender from "@/app/(main)/home/Page";
 export default function Home() {
   return <HomePageRender />;
 }
